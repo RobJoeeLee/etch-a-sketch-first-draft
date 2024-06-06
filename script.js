1eb11bb17b8dda0417448989157a9d4a0cf9f7bd
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2024-06-06 14:12:50 -0400

    Create board function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(16);
+})
+
+function createBoard(size){
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numberOfDivs = size * size;
+
+    for(let i = 0; i < numberOfDivs; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor = "yellow";
+        board.insertAdjacentElement("beforeend", div);
+    }
+}
