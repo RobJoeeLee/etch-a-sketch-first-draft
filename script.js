ea4a8b354ac6f5af7cb1129975ab7784add8e17b
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2024-06-07 08:17:07 -0400

    creating the size of the board

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
+
+    let buttonPopup = document.querySelector("#pop-up");
+    buttonPopup.addEventListener("click", () => {
+        let size = getSize();
+        createBoard(size);
+    })
 })
 
 function createBoard(size){
@@ -14,5 +20,24 @@ function createBoard(size){
         let div = document.createElement("div");
         div.style.backgroundColor = "yellow";
         board.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function getSize(){
+    let input = prompt("What will be the size of the board?")
+    let message = document.querySelector("#message");
+    if(input === ""){
+        message.innerHTML = "Please provide a number";
+    } else if( !/^\d+$/.test(input)){
+        message.innerHTML = "Please provide a number";
+    }
+    else {
+        let gridSize =Number(input);
+        if (gridSize < 1 || gridSize > 100){
+            message.innerHTML = "Provide a number between 1 and 100";
+        } else {
+            message.innerHTML = "Now you can play!";
+        }
+        return input;
     }
 }
